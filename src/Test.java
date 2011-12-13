9eaccbcd95398abdf788020640cd24f06e56efb5
patrickfuerst <patrickfuerst@76-167.swlan.wu-wien.ac.at>
2011-12-13 16:08:30 +0100

    test

--- src/Test.java
+++ src/Test.java
@@ -12,8 +12,15 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Transporter trans1 = new Transporter(4);
+		//Transporter trans1 = new Transporter(4);
 		
+		Animal dolphine = new Dolphin("Peter");
+		Section waterLandTank = new WaterLandTank();
+		Section waterTank = new WaterTank();
+		
+		
+		System.out.println(dolphine.loadTo(waterLandTank) );
+		System.out.println(dolphine.loadTo(waterTank) );
 	}
 
 }
