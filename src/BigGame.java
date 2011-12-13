8d879fc355600099b935a8244d60f9646f54ffaf
Johannes Hofmann-Wellenhof <gianni.hw@gmail.com>
2011-12-13 19:03:59 +0100

    added comments to BigGame

--- src/BigGame.java
+++ src/BigGame.java
@@ -1,9 +1,18 @@
-
+/**
+ * Abstract class BigGame extends Animal
+ * 
+ * @author Gruppe222
+ */
 public abstract class BigGame extends Animal {
 
+	/**
+	 * Standard constructor calls the super constructor
+	 * 
+	 * @param name
+	 *            the name of the BigGame
+	 */
 	public BigGame(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	
